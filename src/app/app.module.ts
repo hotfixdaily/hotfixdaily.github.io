2010f280eebcd3d99ceb779cea4ec5699d439618
Ajitesh Singh <ajiteshjs@gmail.com>
2020-01-19 23:10:15 +0530

    adding postpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "src/app/views/header/header.component";
-import { HomepageComponent } from './views/homepage/homepage.component';
-import { AllPostsComponent } from './views/homepage/all-posts/all-posts.component';
-import { PopularPostsComponent } from './views/homepage/popular-posts/popular-posts.component';
+import { HomepageComponent } from "./views/homepage/homepage.component";
+import { AllPostsComponent } from "./views/homepage/all-posts/all-posts.component";
+import { PopularPostsComponent } from "./views/homepage/popular-posts/popular-posts.component";
+import { PostpageComponent } from "./views/postpage/postpage.component";
+
+const appRoutes: Routes = [
+  {
+    path: "blog",
+    component: PostpageComponent
+  },
+  {
+    path: "**",
+    component: HomepageComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomepageComponent, AllPostsComponent, PopularPostsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    AllPostsComponent,
+    PopularPostsComponent,
+    PostpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/views/postpage/postpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postpage',
+  templateUrl: './postpage.component.html',
+  styleUrls: ['./postpage.component.css']
+})
+export class PostpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
