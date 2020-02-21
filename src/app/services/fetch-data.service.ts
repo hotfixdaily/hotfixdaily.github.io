6d8952e0c25f239826c551f348e74a4b76caa509
ajiteshsingh <ajiteshjs@gmail.com>
2020-02-21 11:36:14 +0530

    categories  header backend integration

--- src/app/services/fetch-data.service.ts
+++ src/app/services/fetch-data.service.ts
@@ -4,7 +4,7 @@ import {
   AngularFirestoreCollection,
   AngularFirestoreDocument
 } from "angularfire2/firestore";
-import { Blog } from "src/app/models/db-collections";
+import { Blog, Category } from "src/app/models/db-collections";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -13,11 +13,14 @@ import { map } from "rxjs/operators";
 })
 export class FetchDataService {
   blogsCollection: AngularFirestoreCollection<Blog>;
+  categoriesCollection: AngularFirestoreCollection<Category>;
   allBlogs: Observable<Blog[]>;
+  allCategories: Observable<Category[]>;
 
   constructor(public afs: AngularFirestore) {
     // this.allBlogs = this.afs.collection("blogs").valueChanges();
     this.blogsCollection = this.afs.collection("blogs");
+    this.categoriesCollection = this.afs.collection("categories");
     this.allBlogs = this.blogsCollection.snapshotChanges().pipe(
       map(changes => {
         return changes.map(a => {
@@ -27,10 +30,27 @@ export class FetchDataService {
         });
       })
     );
+    this.allCategories = this.categoriesCollection.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(a => {
+          const data = a.payload.doc.data() as any;
+          data.id = a.payload.doc.id;
+          const formattedData: Category = {
+            id: data.id,
+            categoryName: data.category_name
+          };
+          return formattedData;
+        });
+      })
+    );
   }
 
   getAllBlogs() {
     return this.allBlogs;
+  }
+
+  getAllCategories() {
+    return this.allCategories;
   }
 
   getBlogDetailsByQuery(q: string) {
